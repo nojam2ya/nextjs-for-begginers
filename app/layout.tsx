c3f7dd27a3a424b5bda93602a496c352d6953879
nojam2ya <jinjoo648@naver.com>
2024-08-28 10:17:11 +0900

    feat: global style 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,16 @@
-import { Metadata } from "next";
-import Navigation from "../components/navigation";
+import { Metadata } from 'next';
+import Navigation from '../components/navigation';
+import '../styles/global.css';
 
 export const metadata: Metadata = {
   title: {
-    template: "%s | Next Movies",
-    default: "Next Movies",
+    template: '%s | Next Movies',
+    default: 'Next Movies',
   },
-  description: "The best movies on the best framework",
+  description: 'The best movies on the best framework',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
